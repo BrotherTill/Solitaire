9b182c85c98583e9649edab770d289232a80cac5
BrotherTill <tilljeromekeb@gmail.com>
2024-04-08 22:04:33 +0200

    combine Elements to functional Game

--- core/src/com/tjirm/solitaire/screens/GameScreen.java
+++ core/src/com/tjirm/solitaire/screens/GameScreen.java
@@ -5,41 +5,68 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.tjirm.solitaire.Solitaire;
 import com.tjirm.solitaire.cards.Card;
 import com.tjirm.solitaire.cards.CardDeck;
 import com.tjirm.solitaire.cards.CardStack;
+import com.tjirm.solitaire.cards.CardStack.DraggableCards;
 import com.tjirm.solitaire.cards.CardStack.RevealedCards;
 import com.tjirm.solitaire.cards.CardType;
+import com.tjirm.solitaire.cards.dragndrop.CardHolder;
 import com.tjirm.solitaire.cards.dragndrop.CardHolderLinker;
-import com.tjirm.solitaire.cards.dragndrop.CardTypeTarget;
-
-import static com.tjirm.solitaire.cards.CardType.CardFace.*;
-import static com.tjirm.solitaire.cards.CardType.Suit.hearts;
-import static com.tjirm.solitaire.cards.CardType.Suit.spades;
 
 public class GameScreen implements Screen {
     Stage stage;
     CardHolderLinker cardHolders;
     
+    private CardStack[] fannedPiles = new CardStack[7];
+    private CardDeck stock;
+    private CardHolder waste;
+    private CardHolder[] foundations = new CardHolder[4];
+    
     public GameScreen() {
         stage = new Stage(new ExtendViewport(800, 600));
         cardHolders = new CardHolderLinker(stage, true);
-        stage.addActor(cardHolders.linkCardHolder(new CardStack(RevealedCards.all, 0, -30)));
-        cardHolders.getCardHolder(0).setPosition(10, 200);
-        cardHolders.getCardHolder(0).addCard(new Card(new CardType(hearts, n10)));
-        stage.addActor(cardHolders.linkCardHolder(new CardStack(RevealedCards.all, 0, -30, true, new CardTypeTarget(false))));
-        cardHolders.getCardHolder(1).setPosition(110, 200);
-        cardHolders.getCardHolder(1).addCard(new Card(new CardType(spades, jack)));
-        cardHolders.getCardHolder(1).addCard(new Card(new CardType(spades, n9)));
-        CardDeck cardDeck = new CardDeck(RevealedCards.none);
-        cardDeck.setTarget(cardHolders.getCardHolder(1));
-        stage.addActor(cardHolders.linkCardHolder(cardDeck));
-        cardHolders.getCardHolder(2).setPosition(210, 200);
-        cardHolders.getCardHolder(2).addCard(new Card(new CardType(spades, queen)));
-        cardHolders.getCardHolder(2).addCard(new Card(new CardType(spades, king)));
         
+        for(int i = 0; i < fannedPiles.length; i++) {
+            stage.addActor(cardHolders.linkCardHolder(fannedPiles[i] = new CardStack(RevealedCards.custom, 0, -30, DraggableCards.revealed, CardHolder.DEFAULT_SOLITAIRE)));
+            fannedPiles[i].setPosition(20 + (Solitaire.preferences.getCardWidth() + 20) * i, stage.getHeight() - 40 - Solitaire.preferences.getCardHeight() * 2);
+        }
+        for(int i = 0; i < foundations.length; i++) {
+            stage.addActor(cardHolders.linkCardHolder(foundations[i] = new CardStack(RevealedCards.all, 0, 0, CardHolder.DEFAULT_FOUNDATIONS)));
+            foundations[i].setPosition(20 + (Solitaire.preferences.getCardWidth() + 20) * i, stage.getHeight() - 20 - Solitaire.preferences.getCardHeight());
+        }
+        
+        stage.addActor(cardHolders.linkCardHolder(waste = new CardStack(RevealedCards.all, 0, 0, CardDeck.DEFAULT_TARGET)));
+        waste.setPosition(20 + (Solitaire.preferences.getCardWidth() + 20) * foundations.length, stage.getHeight() - 20 - Solitaire.preferences.getCardHeight());
+        stage.addActor(cardHolders.linkCardHolder(stock = new CardDeck(RevealedCards.none, 0, 0)));
+        stock.setPosition(20 + (Solitaire.preferences.getCardWidth() + 20) * (foundations.length + 1), stage.getHeight() - 20 - Solitaire.preferences.getCardHeight());
+        stock.setTarget(waste);
+        
+        reset();
+    }
+    
+    public void reset() {
+        Array<Card> cards = new Array<>(52);
+        for(int i = 0; i < CardType.Suit.values().length - 1; i++)
+            for(int j = 0; j < CardType.CardFace.values().length - 1; j++)
+                cards.add(new Card(new CardType(CardType.Suit.values()[i], CardType.CardFace.values()[j])));
+        cards.shuffle();
+        
+        for(CardStack cardStack : fannedPiles)
+            cardStack.setRevealedCards(RevealedCards.top);
+        
+        for(int i = 0; i < fannedPiles.length; i++)
+            for(int j = 0; j <= i; j++)
+                fannedPiles[i].addCard(cards.pop());
+        
+        for(CardStack cardStack : fannedPiles)
+            cardStack.setRevealedCards(RevealedCards.onRemove);
+        
+        stock.addCards(cards.toArray(Card.class));
     }
     
     @Override
