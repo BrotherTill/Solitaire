14269e65e273046cd4e464fdcee43f4d2851967a
BrotherTill <tilljeromekeb@gmail.com>
2024-04-10 20:38:06 +0200

    add GameOver Popup and Restart Button

--- core/src/com/tjirm/solitaire/screens/GameScreen.java
+++ core/src/com/tjirm/solitaire/screens/GameScreen.java
@@ -1,9 +1,16 @@
 package com.tjirm.solitaire.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.tjirm.solitaire.Solitaire;
@@ -15,6 +22,10 @@ import com.tjirm.solitaire.cards.dragndrop.CardHolder;
 import com.tjirm.solitaire.cards.dragndrop.CardHolderLinker;
 import com.tjirm.solitaire.logic.SolitaireGameGenerator;
 import com.tjirm.solitaire.logic.SolitaireWinDetector;
+import com.tjirm.solitaire.preferences.Preferences;
+
+import java.time.Duration;
+import java.time.Instant;
 
 public class GameScreen implements Screen {
     Stage stage;
@@ -25,6 +36,11 @@ public class GameScreen implements Screen {
     private final CardStack waste;
     private final CardDeck stock;
     
+    private Instant start;
+    private final Stack gameOverPopup;
+    private final Label finishTime;
+    private final ImageTextButton restart;
+    
     private final SolitaireWinDetector winDetector;
     private final SolitaireGameGenerator gameGenerator;
     
@@ -33,7 +49,7 @@ public class GameScreen implements Screen {
         cardHolders = new CardHolderLinker(stage, true);
         
         for(int i = 0; i < fannedPiles.length; i++) {
-            stage.addActor(cardHolders.linkCardHolder(fannedPiles[i] = new CardStack(RevealedCards.custom, 0, -30, DraggableCards.revealed, CardHolder.DEFAULT_SOLITAIRE)));
+            stage.addActor(cardHolders.linkCardHolder(fannedPiles[i] = new CardStack(RevealedCards.custom, 0, -30, DraggableCards.revealed, (a,b) -> true)));
             fannedPiles[i].setPosition(20 + (Solitaire.preferences.getCardWidth() + 20) * i, stage.getHeight() - 40 - Solitaire.preferences.getCardHeight() * 2);
         }
         for(int i = 0; i < foundations.length; i++) {
@@ -47,15 +63,61 @@ public class GameScreen implements Screen {
         stock.setPosition(20 + (Solitaire.preferences.getCardWidth() + 20) * (foundations.length + 2), stage.getHeight() - 20 - Solitaire.preferences.getCardHeight());
         stock.setTarget(waste);
         
-        winDetector = new SolitaireWinDetector(fannedPiles, foundations, waste, stock);
+        stage.addActor(winDetector = new SolitaireWinDetector(fannedPiles, foundations, waste, stock));
         winDetector.setOnGameWon(this::gameWon);
         gameGenerator = new SolitaireGameGenerator(fannedPiles, foundations, waste, stock);
         
+        
+        stage.addActor(restart = new ImageTextButton("Restart", Solitaire.UI));
+        restart.clearChildren();
+        restart.stack(restart.getImage(), restart.getLabel());
+        restart.setWidth(120);
+        restart.setPosition(20,20);
+        restart.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                gameGenerator.reset();
+            }
+        });
+        
+        stage.addActor(gameOverPopup  = new Stack());
+        gameOverPopup.addActor(new Image(Solitaire.UI.getDrawable("GameOver_Popup")));
+        gameOverPopup.setSize(250, 170);
+        gameOverPopup.setPosition(stage.getWidth() / 2, -50 , Align.top);
+        VerticalGroup verticalGroup = new VerticalGroup();
+        gameOverPopup.add(new Container<>(verticalGroup).center());
+        verticalGroup.addActor(new Container<>(new Label("You Win!", Solitaire.UI, "head")).pad(20));
+        finishTime = new Label("", Solitaire.UI, "time");
+        verticalGroup.addActor(new Container<>(finishTime).padBottom(30));
+        verticalGroup.getChild(1).setHeight(verticalGroup.getChild(1).getHeight() + 30);
+        ImageTextButton pain = new ImageTextButton("Restart", Solitaire.UI);
+        pain.clearChildren();
+        pain.stack(pain.getImage(), pain.getLabel());
+        verticalGroup.addActor(pain);
+        pain.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                MoveToAction moveToAction = new MoveToAction();
+                moveToAction.setPosition(stage.getWidth() / 2, -50 , Align.top);
+                moveToAction.setDuration(Preferences.MOVE_TO_DURATION);
+                gameOverPopup.addAction(moveToAction);
+                restart.addAction(Actions.moveTo(20, 20, Preferences.MOVE_TO_DURATION));
+                gameGenerator.reset();
+            }
+        });
+        
         gameGenerator.reset();
+        start = Instant.now();
     }
     
     private void gameWon() {
-        Gdx.app.log(getClass().getSimpleName(), "You Win");
+        Duration duration = Duration.between(start, Instant.now());
+        finishTime.setText((duration.getSeconds() / 60) + ":" + (duration.getSeconds() % 60));
+        MoveToAction moveToAction = new MoveToAction();
+        moveToAction.setPosition(stage.getWidth() / 2, stage.getHeight() / 2, Align.center);
+        moveToAction.setDuration(Preferences.MOVE_TO_DURATION);
+        gameOverPopup.addAction(moveToAction);
+        restart.addAction(Actions.moveTo(20, -50, Preferences.MOVE_TO_DURATION));
     }
     
     @Override
@@ -68,6 +130,13 @@ public class GameScreen implements Screen {
         stage.act(delta);
         ScreenUtils.clear(Color.WHITE);
         stage.draw();
+        
+        if(     Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT) &&
+                Gdx.input.isKeyPressed(Input.Keys.SHIFT_RIGHT) &&
+                Gdx.input.isKeyPressed(Input.Keys.NUM_1) &&
+                Gdx.input.isKeyJustPressed(Input.Keys.W)) {
+            gameWon();
+        }
     }
     
     @Override
